5493cf9271923947a9e586afe349d812d43ef4b4
jenkinsl8 <jenkinsl8@users.noreply.github.com>
2026-03-08 06:00:26 -0400

    Set iOS script to use local dev:check command

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -38,4 +38,4 @@ export type RealtimeSessionRequest = {
 export const defaultRealtimeIceServers = [
   { urls: ['stun:stun.l.google.com:19302', 'stun:stun1.l.google.com:19302'] },
   { urls: 'stun:stun.cloudflare.com:3478' }
-] as const;
+];
